996e278012eec53f29b6a4558212b636c739aa39
mikecool89 <79729903+mikecool89@users.noreply.github.com>
2022-06-21 15:31:30 -0700

    Printing X and Y Position list

--- Run_Halo.py
+++ Run_Halo.py
@@ -2,22 +2,6 @@ import cv2
 from algorithms.viclizzy_haarcascade.VicLizzy_Haarcascade import VicLizzy_Haarcascade
 
 if __name__ == "__main__":
-    GREEN = (0, 255, 0)
-    RED = (0, 0, 255)
-    BLACK = (0, 0, 0)
-    YELLOW = (0, 255, 255)
-    WHITE = (255, 255, 255)
-    CYAN = (255, 255, 0)
-    MAGENTA = (255, 0, 242)
-    GOLDEN = (32, 218, 165)
-    LIGHT_BLUE = (255, 9, 2)
-    PURPLE = (128, 0, 128)
-    CHOCOLATE = (30, 105, 210)
-    PINK = (147, 20, 255)
-    ORANGE = (0, 69, 255)
-    
-    fonts = cv2.FONT_HERSHEY_COMPLEX
-    
     Algorithm = VicLizzy_Haarcascade()
     cap = cv2.VideoCapture(0)
 
@@ -28,15 +12,14 @@ if __name__ == "__main__":
 
         # Capture face
         # TODO: enable multiple face capture, create detector object
-        faces = Algorithm.Face_Detector(gray_frame)
+        front_faces = Algorithm.front_face_detector.detectMultiScale(gray_frame, 1.3, 5)
 
-        for (x, y, w, h) in faces:
-            dist_face = Algorithm.Distance_Finder(w)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-            # Drawing Text on the screen
-            cv2.putText(frame, f"Distance: {dist_face:.2f} cm",
-                        (x - 6, y - 6), fonts, 0.5, BLACK, 2)
+        if len(front_faces) != 0:
+            position_list = Algorithm.Get_Front_Face_Position(frame, front_faces)
+            print(position_list)
+            #TODO:
+            # id = Object_Tracker()
+            # Send_Data_To_Flask(id, position_x, position_y)
 
         # show the frame on the screen
         cv2.imshow("frame", frame)
@@ -46,4 +29,3 @@ if __name__ == "__main__":
             cap.release()
             cv2.destroyAllWindows()
             break
-        
